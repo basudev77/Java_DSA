57573c07770c7c916684a48aadcb73b91e6c04c0
BASUDEV DAS <139311324+basudev77@users.noreply.github.com>
2024-07-25 22:07:52 +0530

    Add files via upload

---
+++ TwoDArrays/SpiralMatrix.java
@@ -0,0 +1,56 @@
+package JAVA_DSA.TwoDArrays;
+import java.util.*;
+
+public class SpiralMatrix {
+    public static List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> result = new ArrayList<>();
+        if (matrix == null || matrix.length == 0) {
+            return result;
+        }
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int top = 0, bottom = m - 1, left = 0, right = n - 1;
+        while (top <= bottom && left <= right) {
+            for (int i = left; i <= right; i++) {
+                result.add(matrix[top][i]);
+            }
+            top++;
+            for (int i = top; i <= bottom; i++) {
+                result.add(matrix[i][right]);
+            }
+            right--;
+            if (top <= bottom) {
+                for (int i = right; i >= left; i--) {
+                    result.add(matrix[bottom][i]);
+                }
+                bottom--;
+            }
+
+            if (left <= right) {
+                for (int i = bottom; i >= top; i--) {
+                    result.add(matrix[i][left]);
+                }
+                left++;
+            }
+        }
+        return result;
+    }
+    public static void printArray(int[][] array) {
+        for (int i=0;i<array.length;i++){
+            for(int j=0;j<array[i].length;j++){
+                System.out.print(array[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        int[][]arr={{1,2,3},{4,5,6},{7,8,9}};
+        printArray(arr);
+        List<Integer>ans=spiralOrder(arr);
+        System.out.println("ANSWER-->");
+        for (int i=0;i<ans.size();i++){
+            System.out.print(ans.get(i)+" ");
+        }
+        
+    }
+}
