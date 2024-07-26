092074a35a08cc752a9e27fae707574c6c8bd304
BASUDEV DAS <139311324+basudev77@users.noreply.github.com>
2024-07-26 11:13:23 +0530

    Add files via upload

---
+++ TwoDArrays/NumOfIslands.java
@@ -0,0 +1,37 @@
+package JAVA_DSA.TwoDArrays;
+
+public class NumOfIslands {
+    public static void dfs(char[][] grid,int i,int j){
+        if(i<0 || j<0 || i==grid.length || j==grid[0].length || grid[i][j]!='1'){
+            return;
+        }
+        grid[i][j]='0';
+        dfs(grid,i+1,j);
+        dfs(grid,i-1,j);
+        dfs(grid,i,j+1);
+        dfs(grid,i,j-1);
+    }
+    public static int numIslands(char[][] grid) {
+        int row=grid.length;
+        int col=grid[0].length;
+        int island=0;
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                if(grid[i][j]=='1'){
+                    dfs(grid,i,j);
+                    island++;
+                }
+            }
+        }
+        return island;
+    }
+    public static void main(String[] args) {
+        char[][] grid={
+            {'1','1','0','0','0'},
+            {'1','1','0','0','0'},
+            {'0','0','1','0','0'},
+            {'0','0','0','1','1'}
+        };
+        System.out.println(numIslands(grid));
+    }
+}
---
+++ TwoDArrays/WordSearch.java
@@ -0,0 +1,49 @@
+package JAVA_DSA.TwoDArrays;
+
+public class WordSearch {
+    private static int l, m, n;
+    private static int[][] directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+    
+    private static boolean find(char[][] board, int i, int j, String word, int idx) {
+        if (idx >= l) return true;
+        if (i < 0 || i >= m || j < 0 || j >= n || board[i][j] != word.charAt(idx)) return false;
+        
+        char temp = board[i][j];
+        board[i][j] = '$';  // Mark as visited
+        
+        for (int[] dir : directions) {
+            int i_ = i + dir[0];
+            int j_ = j + dir[1];
+            if (find(board, i_, j_, word, idx + 1)) return true;
+        }
+        
+        board[i][j] = temp;  // Restore the original value
+        return false;
+    }
+    
+    public static boolean exist(char[][] board, String word) {
+        m = board.length;
+        n = board[0].length;
+        l = word.length();
+        if (m * n < 1) return false;
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == word.charAt(0) && find(board, i, j, word, 0)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        char[][] arr = {
+            {'A', 'B', 'C', 'E'},
+            {'S', 'F', 'C', 'S'},
+            {'A', 'D', 'E', 'E'}
+        };
+        String word = "ABCCED";
+        System.out.println(exist(arr, word));
+    }
+}
