dbbc9d617d5b8b544cbc2bcb739f91195ff2fc8b
BASUDEV DAS <139311324+basudev77@users.noreply.github.com>
2024-07-28 10:47:21 +0530

    Add files via upload

---
+++ TwoDArrays/MatWithAltXandO.java
@@ -0,0 +1,50 @@
+package JAVA_DSA.TwoDArrays;
+
+import java.util.Scanner;
+
+public class MatWithAltXandO {
+    public static void createMatrix(int r,int c){
+        char[][]mat=new char[r][c];
+        int row=r,col=c;
+        int lt=0,rt=0;
+        char isX='X';
+        while(lt<r && rt<c){
+            for(int i=lt;i<c;i++){
+                mat[lt][i]=isX;
+            }
+            lt++;
+            for(int j=lt;j<r;j++){
+                mat[j][c-1]=isX;
+            }
+            c--;
+            if (lt < r){
+                for(int k=c-1;k>=rt;k--){
+                    mat[r-1][k]=isX;
+                }
+                r--;
+            }
+            if(rt<c){
+                for(int l=r-1;l>=lt;l--){
+                    mat[l][rt]=isX;
+                }
+                rt++;
+            }
+            isX=(isX=='X')?'O':'X';
+
+        }
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                System.out.print(mat[i][j]+" ");
+            }
+            System.out.println();
+        }
+
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int r=sc.nextInt();
+        int c=sc.nextInt();
+        createMatrix(r,c);
+        sc.close();
+    }
+}
---
+++ TwoDArrays/MaximalRectangle.java
@@ -0,0 +1,77 @@
+package JAVA_DSA.TwoDArrays;
+
+import java.util.Stack;
+
+public class MaximalRectangle {
+    public static int largestRectangleArea(int[] heights) {
+        int n=heights.length;
+        int[] left=new int[n];
+        int[] right=new int[n];
+        
+        Stack<Integer> stk=new Stack<>();
+        for(int i=0;i<n;i++){
+            if(stk.isEmpty()){
+                left[i]=0;
+                stk.push(i);
+            }
+            else{
+                while(!stk.isEmpty() && heights[stk.peek()]>=heights[i]){
+                stk.pop();
+                }
+                left[i]=stk.isEmpty()?0:stk.peek()+1;
+                      stk.push(i);
+            }
+                      
+        }
+        while(!stk.isEmpty()){
+            stk.pop();
+        }
+        for(int j=n-1;j>=0;j--){
+            if(stk.isEmpty()){
+                right[j]=n-1;
+                stk.push(j);
+            }
+            else{
+                while(!stk.isEmpty() && heights[stk.peek()]>=heights[j]){
+                stk.pop();
+                }
+                right[j]=stk.isEmpty()?n-1:stk.peek()-1;
+                      stk.push(j);
+            }
+                      
+        }
+        int max=0;
+        for(int k=0;k<n;k++){
+            max=Math.max(max,(right[k]-left[k]+1)*heights[k]);
+        }
+        return max;
+    }
+    public static int maximalRectangle(char[][] matrix) {
+         if(matrix.length==0){
+            return 0;
+        }
+        int row=matrix.length;
+        int col=matrix[0].length;
+        int[]height=new int[col];
+        for(int i=0;i<col;i++){
+            height[i]=(matrix[0][i]=='1')?1:0;
+        }
+        int maxV=largestRectangleArea(height);
+        for(int j=1;j<row;j++){
+            for(int k=0;k<col;k++){
+                if(matrix[j][k]=='0'){
+                    height[k]=0;
+                }
+                else{
+                    height[k]+=1;
+                }
+            }
+            maxV=Math.max(maxV,largestRectangleArea(height));
+        }
+        return maxV;
+    }
+    public static void main(String[] args) {
+        char[][]matrix={{'1','0','1','0','0'},{'1','0','1','1','1'},{'1','1','1','1','1'},{'1','0','0','1','0'}};
+        System.out.println(maximalRectangle(matrix));
+    }
+}
