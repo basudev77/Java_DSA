c2f70fce162b35292305be20f0e56dec25e4cd2d
BASUDEV DAS <139311324+basudev77@users.noreply.github.com>
2024-07-29 21:19:23 +0530

    Add files via upload

---
+++ Strings/ValidAnagram.java
@@ -0,0 +1,46 @@
+package JAVA_DSA.Strings;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class ValidAnagram {
+    public static boolean isAnagram(String s, String t) {
+       if (s.length() != t.length()) return false;
+        HashMap<Character,Integer>hm=new HashMap<>();
+        for(int i=0;i<s.length();i++){
+            hm.put(s.charAt(i),hm.getOrDefault(s.charAt(i),0)+1);
+        }
+        for(int j=0;j<t.length();j++){
+            if(hm.getOrDefault(t.charAt(j),0)==0)return false;
+            hm.put(t.charAt(j),hm.get(t.charAt(j))-1);
+        }
+        return true;           
+     /* 
+        if (s.length() != t.length()) return false;
+        int alphas[] = new int[26];
+        for(int i=0;i<s.length();i++){
+            char sc=s.charAt(i);
+            char tc=t.charAt(i);
+            alphas[sc-'a']++;
+            alphas[tc-'a']--;
+        }
+        
+        for(int i : alphas){
+            if(i != 0){
+                return false;
+            }
+        }
+        return true;
+    */
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter your 1st string: ");
+        String strA=sc.nextLine();
+        System.out.print("Enter your 2nd string: ");
+        String strB=sc.nextLine();
+        sc.close();
+        System.out.println();
+        System.out.println(isAnagram(strA,strB));
+    }
+}
--- Strings/ValidPalindrome.java
+++ Strings/ValidPalindrome.java
@@ -1,1 +1,26 @@
+package JAVA_DSA.Strings;
 
+import java.util.Scanner;
+
+public class ValidPalindrome {
+    public static boolean isPalindrome(String s) {
+        int i=0,j=s.length()-1;
+        s=s.toLowerCase();
+        while(i<j){
+            
+            while(i<j && !((s.charAt(i)>='a' && s.charAt(i)<='z')|| (s.charAt(i)>='0' && s.charAt(i)<='9')))i++;
+            while(i<j && !((s.charAt(j)>='a' && s.charAt(j)<='z')|| (s.charAt(j)>='0' && s.charAt(j)<='9')))j--;
+            if(s.charAt(i)!=s.charAt(j))return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter your string: ");
+        String str=sc.nextLine();
+        sc.close();
+        System.out.println(isPalindrome(str));
+    }
+}
