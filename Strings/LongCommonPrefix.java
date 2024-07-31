82de26273aa8dee6210bd1d7031141b58aac4e5d
BASUDEV DAS <139311324+basudev77@users.noreply.github.com>
2024-07-31 21:04:57 +0530

    Add files via upload

---
+++ Strings/LongCommonPrefix.java
@@ -0,0 +1,22 @@
+package JAVA_DSA.Strings;
+public class LongCommonPrefix {
+    public static String longestCommonPrefix(String[] strs) {
+        String res="";
+        for(int i=0;i<strs[0].length();i++){
+            char curr=strs[0].charAt(i);
+            if(check(strs,curr,i))res+=curr;
+            else break;
+        }
+        return res;
+    }
+    public static boolean check(String[] strs,char curr,int idx){
+        for(int i=1;i<strs.length;i++){
+            if(idx >= strs[i].length() || strs[i].charAt(idx) != curr) return false;
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        String[] arr={"flower","flow","flight"};
+        System.out.println(longestCommonPrefix(arr));
+    }
+}
