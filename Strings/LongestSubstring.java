47ca9d19f9784f9cfdfaac57db4be256fd8044c0
BASUDEV DAS <139311324+basudev77@users.noreply.github.com>
2024-08-01 21:12:45 +0530

    Add files via upload

---
+++ Strings/LongestSubstring.java
@@ -0,0 +1,22 @@
+package JAVA_DSA.Strings;
+
+import java.util.HashMap;
+
+public class LongestSubstring {
+    public static int lengthOfLongestSubstring(String s) {
+        int right, left = 0, max = 0;
+        HashMap<Character, Integer> map = new HashMap<>();
+        for(right = 0; right < s.length(); right++) {
+            if(map.containsKey(s.charAt(right))) {
+               max = Math.max(max, right - left);
+               left = Math.max(left, map.get(s.charAt(right)) + 1); 
+            }  
+             map.put(s.charAt(right), right);
+        }
+        return Math.max(max, right - left);
+    }
+    public static void main(String[] args) {
+        String s="basudevdas";
+        System.out.println(lengthOfLongestSubstring(s));
+    }
+}
