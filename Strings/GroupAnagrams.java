ce13820e421d7775ee09784dff930c1d33c9ba26
BASUDEV DAS <139311324+basudev77@users.noreply.github.com>
2024-08-03 21:51:11 +0530

    Add files via upload

---
+++ Strings/GroupAnagrams.java
@@ -0,0 +1,43 @@
+package JAVA_DSA.Strings;
+
+import java.util.*;
+
+public class GroupAnagrams {
+    private static String generate(String s) {
+        int[] count = new int[26];
+
+        for (char ch : s.toCharArray()) {
+            count[ch - 'a']++;
+        }
+        StringBuilder newS = new StringBuilder();
+
+        for (int i = 0; i < 26; i++) {
+            while (count[i] > 0) {
+                newS.append((char) (i + 'a'));
+                count[i]--;
+            }
+        }
+        return newS.toString();
+    }
+
+    public static List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+
+        for (String s : strs) {
+            String newS = generate(s);
+
+            if (!map.containsKey(newS)) {
+                map.put(newS, new ArrayList<>());
+            }
+
+            map.get(newS).add(s);
+        }
+
+        return new ArrayList<>(map.values());
+    }
+    public static void main(String[] args) {
+        String[] str={"eat","tea","tan","ate","nat","bat"};
+        List<List<String>>ans=groupAnagrams(str);
+        System.out.println(ans);
+    }
+}
