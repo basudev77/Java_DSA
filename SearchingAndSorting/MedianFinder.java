4f89dcb541dde866e590b09e43b707f9a8b45fdd
BASUDEV DAS <dbasudev329@gmail.com>
2024-09-25 16:45:56 +0530

    Updated

---
+++ SearchingAndSorting/MedianFinder.java
@@ -0,0 +1,45 @@
+package SearchingAndSorting;
+import java.util.*;
+public class MedianFinder {
+    private PriorityQueue<Integer> leftMaxHeap; // max heap
+    private PriorityQueue<Integer> rightMinHeap; // min heap
+
+    public MedianFinder() {
+        leftMaxHeap = new PriorityQueue<>(Collections.reverseOrder());
+        rightMinHeap = new PriorityQueue<>();
+    }
+
+    public void addNum(int num) {
+        if (leftMaxHeap.isEmpty() || num < leftMaxHeap.peek()) {
+            leftMaxHeap.add(num);
+        } else {
+            rightMinHeap.add(num);
+        }
+
+        // always maintain leftMaxHeap size one greater than rightMinHeap size
+        // or both sizes equal
+        if (Math.abs(leftMaxHeap.size() - rightMinHeap.size()) > 1) {
+            rightMinHeap.add(leftMaxHeap.poll());
+        } else if (leftMaxHeap.size() < rightMinHeap.size()) {
+            leftMaxHeap.add(rightMinHeap.poll());
+        }
+    }
+
+    public double findMedian() {
+        if (leftMaxHeap.size() == rightMinHeap.size()) {
+            // even number of elements
+            return (double) (leftMaxHeap.peek() + rightMinHeap.peek()) / 2;
+        }
+
+        // odd number of elements
+        return leftMaxHeap.peek();
+    }
+    public static void main(String[] args) {
+        MedianFinder finder = new MedianFinder();
+        finder.addNum(1);
+        finder.addNum(2);
+        System.out.println("Median: " + finder.findMedian()); // 1.5
+        finder.addNum(3);
+        System.out.println("Median: " + finder.findMedian()); // 2
+    }
+}
